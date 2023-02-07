84e5b271b4388b1eb4c3c1f10921e6ba2bcd9399
Andrew aisr <andro.aisr681@gmail.com>
2023-02-07 06:57:07 +0200

    adding social icons

--- src/screens/contact/Contact.js
+++ src/screens/contact/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './contact.css'
  import {navigationLinks} from '../../helpers/navigationLinks'
+ import { SocialIcon } from 'react-social-icons';
 function createLinks() {
     return navigationLinks.map((e, idx) => (
       <p key={idx}><a href={e.ref}>{e.name}</a></p>
@@ -14,7 +15,12 @@ function createLinks() {
           <div>
             <p>Egypt, Cairo</p>
             <p>Andrew Aisr Adolf</p>
-            <p>Andrawesaisr@gmail.com</p>
+            <p>andrawesaisr@gmail.com</p>
+            <div className='social_icons'>
+            <SocialIcon url="https://www.linkedin.com/in/andrew-aisr-49696220b/" target="_blank" />
+            <SocialIcon url="https://github.com/Andrawesaisr?tab=repositories"  bgColor="white" target="_blank"/>
+            <SocialIcon url="https://leetcode.com/andrewAisr/"  bgColor="white" target="_blank" />
+            </div>
           </div>
           <div>
             {createLinks()}
